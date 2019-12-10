724047c997fcaaa192048f05c7cdda1ff55cacbf
Pen-y-Fan <{ID}+{username}@users.noreply.github.com>
2019-12-10 00:03:32 +0000

    Product seeder

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Product::class, 10)->create();
+        $this->command->info('Ten Products created');
+        gc_collect_cycles();
+    }
+}
