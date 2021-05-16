239c0199fb9e121af22432b8331caf1f09537726
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2021-05-16 12:36:00 +0100

    Add HasFactory

--- app/Product.php
+++ app/Product.php
@@ -2,10 +2,19 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Product
+ * @package App
+ * @method static paginate
+ * @method static create(array $product)
+ * @method static findOrFail($id)
+ */
 class Product extends Model
 {
+    use HasFactory;
     /**
      * The attributes that are mass assignable.
      *
